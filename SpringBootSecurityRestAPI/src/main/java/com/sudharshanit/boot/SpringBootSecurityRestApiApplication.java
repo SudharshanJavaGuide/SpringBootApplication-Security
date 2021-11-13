54bcb8e5d13399c9c523232afa395e14be01dbbd
Sudharshan <sudharshanjavaguide@gmail.com>
2021-11-13 18:39:33 +0530

    Project Directory Initialization

---
+++ SpringBootSecurityRestAPI/src/main/java/com/sudharshanit/boot/SpringBootSecurityRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sudharshanit.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityRestApiApplication.class, args);
+	}
+
+}
---
+++ SpringBootSecurityRestAPI/src/test/java/com/sudharshanit/boot/SpringBootSecurityRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudharshanit.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
